90e1704de9a8eb2f56519029469615fd2a9232ca
lesponns <71814927+lesponns@users.noreply.github.com>
2023-07-28 07:08:18 +0300

    Update Program.cs

--- SmartPro_7/Program.cs
+++ SmartPro_7/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +10,23 @@ namespace SmartPro_7
     {
         static void Main(string[] args)
         {
+            double item_price, last_price;
+
+            Console.Write("Ürün fiyatı: ");
+            item_price = double.Parse(Console.ReadLine());
+
+            if (item_price < 100 && item_price > 0)
+            {
+                Console.Clear();
+                last_price = item_price + 35;
+                Console.WriteLine("Kargo ücreti 35tl dir. Toplam borcunuz: "+last_price+"tl'dir.");
+            }
+            else if (item_price >= 100)
+            {
+                Console.Clear();
+                Console.WriteLine("Kargo ücreti yoktur. \nToplam borcunuz: " + item_price +"tl'dir.");
+            }
+            Console.ReadKey();
         }
     }
 }
